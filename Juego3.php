2bca22ee911191b9dbdadaae1919d18472c98a55
xp <xp@Android029>
2016-10-24 16:08:31 +0200

    Cambiado algunas cosas para que todo funcione.

--- Juego3.php
+++ Juego3.php
@@ -1,120 +1,45 @@
+<?php
 
 
+
+
+?>
 <html>
-     <?php
-         
-           session_start();
-          
-        ?>
     <head>
-        <meta charset="UTF-8">
-        <title>Juego3</title>
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <link href="css/desplegable/CSSJuego1.css" rel="stylesheet" type="text/css">
-        <link href="css/desplegable/transfomracion.css" rel="stylesheet" type="text/css">
-        <script src="css/desplegable/JQueryLatest.js"></script>
-        <script src="css/desplegable/main.js"></script>
-        <script src="js/Random.js"></script>
-        
-        <style>
-            body{
-                    background-image: url(imagenes/ciudad.jpg);
-
-            }
-			.zoom{
-                            /* Aumentamos la anchura y altura durante 2 segundos */
-                            transition: width 2s, height 2s, transform 2s;
-                            -moz-transition: width 2s, height 2s, -moz-transform 2s;
-                            -webkit-transition: width 2s, height 2s, -webkit-transform 2s;
-                            -o-transition: width 2s, height 2s,-o-transform 2s;
-                            }
-   			.zoom:hover{
-                            /* tranformamos el elemento al pasar el mouse por encima */
-                            transform : scale(2);
-                            -moz-transform : scale(2);      /* Firefox */
-                            -webkit-transform : scale(2);   /* Chrome - Safari */
-                            -o-transform : scale(2);        /* Opera */
-                            }
-
-            
-        </style>
-    </head>
-    <body >
-        <div class="container" >
-            <div class="row">
-                <!--<Este es la primera fila que incluye el titulo Histologia >-->
-                <div class="col-md-18"><h2 class="text-center" style="color:white;">   <br>  </h2>
-                <div class="col-md-12"><h2 class="text-center" style="color:white;">HISTOLOG?A</h2>
-                <!--<Aqui empieza el menu de la pagina >-->
-                <header>
-		<div class="menu_bar">
-			<a href="#" class="bt-menu">Hitoslogía</a>
-		</div>
-
-		<nav>
-			<ul>
-            <li><a href="inicio.php">Inicio</a></li>
-				<li class="submenu">
-					<a href="#">Juegos</a>
-					<ul class="children">
-                        <li><a href="Juego1.php">Juego 1</a></li>
-                        <li><a href="Juego2.php">Juego 2</a></li>
-						<li><a href="Juego4.php">Juego 4</a></li>
-					</ul>
-				</li>
-				<li><a href="resultados.html">Resultados</a></li>
-                <li><a href="#" onClick="getRandom()" id="random">Random</a></li>
-                                                       
-				<ul class="nav navbar-nav navbar-right">          
-           <li><a href="index_1.php" >Cerrar sesi?n </a></li>
-            <img src="imagenes/alumnos/<?php echo $_SESSION['username']; ?>.jpg" 
-                 class="img-circle" style="width:55px;padding:10px;">
-          
-			</ul>	</ul>
-				
-			
-		</nav>
-	</header>
-                <!--<Fin menu>-->
-                
-                </div>
-            </div>
-            
-            <div class="col-md-18"><h2 class="text-center" style="color:white;">Juego 3</h2>
-                <div class="col-md-18"><h2 class="text-center" style="color:white;">   <br>  </h2>
-                  
-            <div class="row">
-                <div class="col-md-12">
-                    
-                     
-                     
-                <div class="col-md-18">
-                    <div class="thumbnail">			<!--Imagen base de datos-->
-                        
-                            <img src="imagenes/imagen0021.jpg" style="width: 50%; border: Black 5px solid;
-                            border-radius: 20px;" class="zoom" >
-                    										<!--Respuestas Posibles-->
-
-                     </div>
-                    <button type="button" class="btn btn-primary btn-lg btn-block">Block level button</button>
-                    <button type="button" class="btn btn-primary btn-lg btn-block">Block level button</button>
-                    <button type="button" class="btn btn-primary btn-lg btn-block">Block level button</button>
-                    <button type="button" class="btn btn-primary btn-lg btn-block">Block level button</button>
-                    <br>
-                    <br>
-
-                    
-                    <h2 class="text-center" style="color:white;">  </h2>
-                    <div class="row"> 
-                </div>
-                <div class="col-md-4">
-                    
-                </div>
-                <div class="col-md-4"></div>
-            	</div>
-        </div>
-        </div>
-    </body>
+     
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link href="css/desplegable/CSSJuego1.css" rel="stylesheet" type="text/css">
+    <link href="css/desplegable/transfomracion.css" rel="stylesheet" type="text/css">
+    <script src="css/desplegable/JQueryLatest.js"></script>
+    <script src="css/desplegable/main.js"></script>
+    <script src="js/Random.js"></script>
     <script src="js/jquery-3.1.0.min.js"></script>
     <script src="js/bootstrap.js"></script>
-                </html>
+        <style>
+.boton{
+    font-family:cursive;       
+    text-align: center;
+	margin: 0 auto;
+}
+</style>
+<script type="text/javascript">
+
+function siguiente() { //Hacer un random no repeat con juego 3
+
+   
+    }
+
+
+//function anterior() {
+//     
+//    }
+</script>
+</head>
+<body>
+    <iframe id="IFrame1" src="juego3actual.php" width="100%" height="97%"></iframe><!--Enlazar con juego 3-->
+    
+<!--    <input type="button" class="boton"  value="Anterior" onClick="return ChangePage()" />-->
+    <input type="button" class="boton-btn-primary btn-block" class="" value="Siguiente" onClick="return siguiente()" />
+
+</body>
+</html>
